9e4cbd194d8009955d61da30cf9ac608d734d8fc
Rin 🌸 <rin-z4@proton.me>
2025-05-13 10:47:14 +0200

    Add basic types header

---
+++ src/types.h
@@ -0,0 +1,20 @@
+#ifndef __STS_TYPES_H_RNnBVA4C
+#define __STS_TYPES_H_RNnBVA4C
+
+/* Shorthands */
+typedef unsigned char uchar;
+typedef   signed char schar;
+
+typedef unsigned short ushort;
+typedef   signed short sshort;
+
+typedef unsigned int   uint;
+typedef   signed int   sint;
+
+typedef unsigned long  ulong;
+typedef   signed long  slong;
+
+/* Boolean type - smallest unsigned */
+typedef uchar bool_t;
+
+#endif
